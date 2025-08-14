d9585b31d1d63e31eb1218eff1556979c8aff4e2
Dineury <DineuryLopez1@gmail.com>
2025-08-14 19:34:33 -0400

    finished MP

--- frontend/state/quotesSlice.js
+++ frontend/state/quotesSlice.js
@@ -1,4 +1,7 @@
 // ✨ create your `quotesSlice` in this module
+import { createSlice } from "@reduxjs/toolkit"
+
+
 
 let id = 1
 const getNextId = () => id++
@@ -26,3 +29,50 @@ const initialState = {
     },
   ],
 }
+
+export const quotesSlice = createSlice({
+  name: 'quotes',
+  initialState,
+  reducers: {
+    toggleVisibility(state, action){
+      state.displayAllQuotes = !state.displayAllQuotes
+    },
+    deleteQuote(state, action){
+      state.quotes = state.quotes
+      .filter(qt => qt.id !== action.payload )
+    },
+    editQuoteAuthenticity(state, action){
+      const quoteToEdit = state.quotes.find(qt => qt.id === action.payload)
+      quoteToEdit.apocryphal = !quoteToEdit.apocryphal
+    },
+    setHighlightedQuote(state, action){
+      state.highlightedQuote !== action.payload ? state.highlightedQuote = action.payload : state.highlightedQuote = null
+    
+    },
+    createQuote : {
+      prepare({authorName, quoteText}) {
+        return {
+          payload:{
+            authorName,
+            quoteText,
+            apocryphal: false,
+            id: getNextId()
+          }
+        }
+      },
+      reducer(state, action) {
+        state.quotes.push(action.payload)
+      }
+    }
+  }
+})
+
+export const {
+toggleVisibility,
+deleteQuote,
+editQuoteAuthenticity,
+setHighlightedQuote,
+createQuote,
+} = quotesSlice.actions
+
+export default quotesSlice.reducer
